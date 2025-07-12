22bfbec1cd8e1848449818d4fdc031c2f6998a86
Tomáš Kudělka <tomas@appsdevteam.com>
2025-07-12 18:13:00 +0200

    Allows access to presenter parameters
    Makes it possible to access presenter parameters
    through the trait, rather than only through
    the presenter itself. This improves flexibility.

--- src/UI/Presenters/SidePanel.php
+++ src/UI/Presenters/SidePanel.php
@@ -18,6 +18,7 @@ trait SidePanel
 	abstract protected function getQueryObject(): QueryObject;
 	abstract protected function getPresenter(): ?Presenter;
 	abstract public function getSnippetId(string $name): string;
+	abstract public function getParameter(string $name): mixed;
 
 	/**
 	 * @throws ReflectionException
@@ -25,7 +26,8 @@ trait SidePanel
 	public function createComponentSidePanel(SidePanelControlFactory $factory): SidePanelControl
 	{
 		$entity = $this->getEntity();
-		if ($editId = $this->getPresenter()->getParameter('editId')) {
+
+		if ($editId = $this->getParameter('editId')) {
 			$entity = $this->getQueryObject()->byId($editId)->fetchOne();
 		}
 
