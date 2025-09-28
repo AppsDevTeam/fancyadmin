c071c6e492ad64710ef6f36f5da3f48ce457eafb
Tomáš Kudělka <tomas@appsdevteam.com>
2025-09-28 19:38:38 +0200

    Adds login method to SecurityUserTrait
    Adds the `login` method to the `SecurityUserTrait`.
    
    This allows to call parent::login() from within trait implementation.

--- src/Model/Security/SecurityUserTrait.php
+++ src/Model/Security/SecurityUserTrait.php
@@ -3,7 +3,9 @@
 namespace ADT\FancyAdmin\Model\Security;
 
 use ADT\FancyAdmin\Model\Entities\AclRole;
+use Nette\Security\AuthenticationException;
 use Nette\Security\Authorizator;
+use Nette\Security\IIdentity;
 
 trait SecurityUserTrait
 {
@@ -13,5 +15,19 @@ trait SecurityUserTrait
 			$this->getIdentity()->getRoles(),
 			fn(AclRole $role) => $role->getIsAdmin() || $this->getAuthorizator()->isAllowed($role->getRoleId(), $resource, $privilege)
 		);
+	}
+
+	/**
+	 * @throws AuthenticationException
+	 */
+	public function login(
+		string|IIdentity $username,
+		#[\SensitiveParameter]
+		?string $password = null,
+		?string $context = null,
+		array $metadata = []
+	): void
+	{
+		parent::login($username, $password, $context, $metadata);
 	}
 }
