d03672f89767adddc521d8f6114bcef14d1546d8
Pavel Konvička <pavel.konvicka@gmail.com>
2025-10-02 15:56:28 +0200

    Add base grid

---
+++ src/UI/Components/Grids/BaseGridTrait.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace ADT\FancyAdmin\UI\Components\Grids;
+
+use ADT\BackgroundQueue\BackgroundQueue;
+use ADT\Datagrid\Component\DataGrid;
+use ADT\DoctrineComponents\Entities\Entity;
+use ADT\DoctrineComponents\QueryObject\QueryObject;
+use ADT\DoctrineForms\BaseForm;
+use ADT\FancyAdmin\DI\Injects\TranslatorInject;
+use ADT\FancyAdmin\UI\Presenters\SidePanel;
+use ADT\QueryObjectDataSource\IQueryObjectDataSourceFactory;
+use App\Model\Doctrine\EntityManager;
+use App\Model\Entities\GridFilter;
+use App\Model\Filters;
+use App\Model\Queries\Factories\GridFilterQueryFactory;
+use App\Model\Security\SecurityUser;
+use App\UI\Portal\Components\Forms\GridFilter\GridFilterFormFactory;
+use Kdyby\Autowired\Attributes\Autowire;
+use Nette\Application\AbortException;
+use Nette\Localization\Translator;
+use Nette\Security\User;
+
+trait BaseGridTrait
+{
+	use SidePanel;
+	use TranslatorInject;
+
+	#[Autowire]
+	public IQueryObjectDataSourceFactory $queryObjectDataSource;
+
+	#[Autowire]
+	public SecurityUser $securityUser;
+
+	#[Autowire]
+	public EntityManager $em;
+
+	#[Autowire]
+	public GridFilterQueryFactory $gridFilterQueryFactory;
+
+	#[Autowire]
+	public GridFilterFormFactory $gridFilterFormFactory;
+
+	#[Autowire]
+	public BackgroundQueue $backgroundQueue;
+
+	#[Autowire]
+	public Filters $filters;
+
+	public ?GridFilter $entity = null;
+
+	public function getEntityManager(): EntityManager
+	{
+		return $this->em;
+	}
+
+	public function getTranslator(): Translator
+	{
+		return $this->_translator;
+	}
+
+	public function getGridFilterQueryFactory(): GridFilterQueryFactory
+	{
+		return $this->gridFilterQueryFactory;
+	}
+
+	public function getSecurityUser(): User
+	{
+		return $this->securityUser;
+	}
+
+	public function getQueryObjectDataSourceFactory(): IQueryObjectDataSourceFactory
+	{
+		return $this->queryObjectDataSource;
+	}
+
+	public function getDataGridClass(): string
+	{
+		return DataGrid::class;
+	}
+
+	public function getEntity(): callable|null|Entity
+	{
+		return $this->entity;
+	}
+
+	public function getForm(): BaseForm
+	{
+		return $this->gridFilterFormFactory->create()
+			->setGrid($this);
+	}
+
+	public function getQueryObject(): QueryObject
+	{
+		return $this->gridFilterQueryFactory->create();
+	}
+
+	/**
+	 * @throws AbortException
+	 */
+	public function redrawSidePanel(): never
+	{
+		$this->getPresenter()->payload->snippets[$this->getPresenter()->getSnippetId('sidePanel')] = $this['sidePanel']->renderToString();
+		$this->getPresenter()->sendPayload();
+	}
+
+	public function handleEditAdvancedFilter(): void
+	{
+		$this->redrawSidePanel();
+	}
+
+	public function getEmail(): string
+	{
+		return $this->securityUser->getIdentity()->getEmail();
+	}
+}
