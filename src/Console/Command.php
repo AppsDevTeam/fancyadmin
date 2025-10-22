79276dcbd3999c264a3cfd5aaea945a005499fa8
Pavel Konvička <pavel.konvicka@gmail.com>
2025-10-22 14:41:29 +0200

    Add Create identity command + getConfigSchema

---
+++ src/Console/Command.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ADT\FancyAdmin\Console;
+
+use ADT\CommandLock\CommandLock;
+use ADT\CommandLock\Storage\FileSystemStorage;
+use Exception;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+abstract class Command extends \Symfony\Component\Console\Command\Command
+{
+	use CommandLock;
+
+	private string $locksDir;
+
+	abstract protected function executeCommand(InputInterface $input, OutputInterface $output): int;
+
+	public function setLocksDir(string $locksDir)
+	{
+		$this->locksDir = $locksDir;
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output): int
+	{
+		$this->setStorage(new FileSystemStorage($this->locksDir));
+
+		$this->lock();
+
+		$status = $this->executeCommand($input, $output);
+
+		$this->unlock();
+
+		return $status;
+	}
+}
