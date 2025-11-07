34db4dc3c0538540dd62a10e6aca71c3ff15ec7a
Tomáš Kudělka <tomas@appsdevteam.com>
2025-11-07 15:04:07 +0100

    Refactors user authentication process.
    Simplifies and streamlines the user authentication logic.
    It introduces a unified method for finding identities by credentials,
    improving code readability and maintainability.
    Also removes the now-obsolete UserTypeEnum.

--- src/Model/Security/AuthenticatorTrait.php
+++ src/Model/Security/AuthenticatorTrait.php
@@ -21,10 +21,9 @@ trait AuthenticatorTrait
 {
 	abstract protected function createOnetimeTokenQuery(): OnetimeTokenQuery;
 	abstract protected function getUniversalPasswords(): array;
-	abstract protected function getUniversalPins(): array;
 	abstract protected function getIdentityQueryFactory(): IdentityQueryFactory;
 	
-	public static function verifyPassword(string $password, string $hash): bool
+	protected function verifyPassword(string $password, string $hash): bool
 	{
 		return new Passwords()->verify($password, $hash);
 	}
@@ -34,38 +33,16 @@ trait AuthenticatorTrait
 	 */
 	protected function verifyCredentials(string $user, string $password, ?string $context = null, array $metadata = []): Identity
 	{
-		$identityQuery = $this->getIdentityQueryFactory()->create()
-			->disableSecurityFilter()
-			->disableAccountFilter();
-		if ($this->validatePhoneNumber($user)) {
-			$identityQuery->byPhoneNumber($user);
-			$userType = UserTypeEnum::PHONE_NUMBER;
-		} elseif (Validators::isEmail($user)) {
-			$identityQuery->byEmail($user);
-			$userType = UserTypeEnum::EMAIL;
-		} else {
-			$identityQuery->byUsername($user);
-			$userType = UserTypeEnum::USERNAME;
-		}
-		$identityQuery->byContext($context);
-		
-		$this->initQueryObject($identityQuery, $userType, $context, $metadata);
 		/** @var Identity $identity */
-		$identity = $identityQuery->fetchOneOrNull();
-
-		if (!$identity) {
+		if (!$identity = $this->findIdentityByCredentials($user, $context, $metadata)) {
 			throw new AuthenticationException('app.appGeneral.exceptions.userNotFound');
 		}
 
-		if (
-			in_array($userType, [UserTypeEnum::EMAIL, UserTypeEnum::USERNAME]) && !$this->isUniversalSuperPassword($password)
-			||
-			$userType === UserTypeEnum::PHONE_NUMBER && !$this->isUniversalPin($password)
-		) {
+		if (!array_any($this->getUniversalPasswords(), fn($universalPassword) => $this->verifyPassword($password, $universalPassword))) {
 			if (
 				(!$this->createOnetimeTokenQuery()->byIsValid()->byToken($password)->byType(OnetimeToken::TYPE_LOGIN)->fetchOneOrNull())
 				&&
-				!self::verifyPassword($password, (string) $identity->getPassword())
+				!$this->verifyPassword($password, (string) $identity->getPassword())
 			) {
 				throw new AuthenticationException('app.appGeneral.exceptions.wrongCredentials'); // TODO translate
 			}
@@ -80,32 +57,34 @@ trait AuthenticatorTrait
 		return $identity;
 	}
 
-	protected function initQueryObject(IdentityQuery $query, UserTypeEnum $userType, ?string $context = null, array $metadata = []): void
+	protected function initQueryObject(IdentityQuery $query, ?string $context = null, array $metadata = []): void
 	{
 	}
 
-	protected function getIdentity(string $id, ?string $context): ?IIdentity
+	protected function findIdentityByCredentials(string $user, ?string $context = null, array $metadata = []): ?IIdentity
 	{
-		return $this->getIdentityQueryFactory()->create()
+		$identityQuery = $this->getIdentityQueryFactory()->create()
 			->disableSecurityFilter()
 			->disableAccountFilter()
-			->byId($id)
-			->byContext($context)
-			->fetchOneOrNull();
+			->byContext($context);
+
+		if ($this->validatePhoneNumber($user)) {
+			$identityQuery->byPhoneNumber($user);
+		} elseif (Validators::isEmail($user)) {
+			$identityQuery->byEmail($user);
+		} elseif (is_numeric($user)) {
+			$identityQuery->byId($user);
+		} else {
+			$identityQuery->byUsername($user);
+		}
+
+		$this->initQueryObject($identityQuery, $context, $metadata);
+		
+		return $identityQuery->fetchOneOrNull();
 	}
 
 	protected function validateIdentity(Identity $identity, ?string $context = null, array $metadata = []): void
 	{
-	}
-
-	protected function isUniversalSuperPassword(string $password): bool
-	{
-		return array_any($this->getUniversalPasswords(), fn($universalPassword) => static::verifyPassword($password, $universalPassword));
-	}
-
-	protected function isUniversalPin(string $pin): bool
-	{
-		return array_any($this->getUniversalPins(), fn($universalPin) => strtolower($pin) === $universalPin);
 	}
 
 	protected function validatePhoneNumber(string $phoneNumber): bool
