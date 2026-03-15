ba75932f63aabb8ec1f410335e82120b16efbb5b
Tomáš Kudělka <tomas@appsdevteam.com>
2026-03-15 09:27:57 +0100

    refactor configuration grid trait and update entity namespace
    - Update the Configuration entity reference to the common ADT namespace.
    - Remove hardcoded `allowEdit` and `allowDelete` methods to align with the new modular, action-based permission validation system.

--- src/UI/Components/Grids/Configuration/ConfigurationGridTrait.php
+++ src/UI/Components/Grids/Configuration/ConfigurationGridTrait.php
@@ -5,13 +5,10 @@ declare(strict_types=1);
 namespace ADT\FancyAdmin\UI\Components\Grids\Configuration;
 
 use ADT\Datagrid\Component\DataGrid;
-use ADT\Datagrid\Component\DeleteParams;
-use ADT\Datagrid\Component\EditParams;
-use ADT\FancyAdmin\Model\Entities\Enums\AclResourceNameEnum;
+use ADT\FancyAdmin\Model\Entities\Configuration;
 use ADT\FancyAdmin\Model\Entities\Enums\ConfigurationTypeEnum;
 use ADT\FancyAdmin\Model\Queries\Factories\ConfigurationQueryFactory;
 use ADT\FancyAdmin\UI\Components\Grids\Traits\Editable\Editable;
-use App\Model\Entities\Configuration;
 use Nette\Utils\Json;
 
 trait ConfigurationGridTrait
@@ -30,16 +27,6 @@ trait ConfigurationGridTrait
 				}
 				return $configuration->getValue();
 			});
-	}
-
-	protected function allowEdit(): ?EditParams
-	{
-		return new EditParams($this->aclResource, 'edit!');
-	}
-
-	protected function allowDelete(): ?DeleteParams
-	{
-		return null;
 	}
 
 	protected function getQueryObjectFactoryClass(): string
