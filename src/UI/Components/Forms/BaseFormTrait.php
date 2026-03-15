2991d7f8e7d52d7f9dd76ba38bb269918cf4a04f
Tomáš Kudělka <tomas@appsdevteam.com>
2026-03-15 09:26:01 +0100

    add automatic account field injection to entity forms
    - Use reflection to detect if the underlying entity has an `account` property.
    - If multiple accounts exist, automatically inject a required select field into the form with hierarchical labeling.
    - Support disabling this behavior via the `$disableAccountInput` property.

--- src/UI/Components/Forms/BaseFormTrait.php
+++ src/UI/Components/Forms/BaseFormTrait.php
@@ -3,17 +3,49 @@
 namespace ADT\FancyAdmin\UI\Components\Forms;
 
 use ADT\DoctrineForms\Form;
+use ADT\FancyAdmin\DI\Injects\AccountQueryFactoryInject;
 use ADT\FancyAdmin\DI\Injects\EntityManagerInject;
 use ADT\FancyAdmin\DI\Injects\TranslatorInject;
 use ADT\FancyAdmin\UI\Components\Controls\SidePanel\SidePanelSize;
 use ADT\FancyAdmin\UI\Components\ControlTrait;
 use ADT\Forms\BootstrapFormRenderer;
+use App\UI\Portal\Components\Forms\Base\EntityForm;
 
 trait BaseFormTrait
 {
 	use ControlTrait;
 	use EntityManagerInject;
 	use TranslatorInject;
+	use AccountQueryFactoryInject;
+
+	protected bool $disableAccountInput = false;
+
+	/**
+	 * @throws \ReflectionException
+	 */
+	public function onBeforeInitForm(Form $form): void
+	{
+		if ($this->getEntityClass() && !$this->disableAccountInput) {
+			$refClass = new \ReflectionClass($this->getEntityClass());
+
+			if ($refClass->hasProperty('account')) {
+				if ($this->_accountQueryFactory->create()->count() > 1) {
+					$pairs = [];
+					foreach ($this->_accountQueryFactory->create()->orderBy(['parent' => 'ASC', 'name' => 'ASC'])->fetch() as $_account) {
+						if ($_account->getParent()) {
+							$pairs[$_account->getId()] = '-- ' . $_account->getName();
+						} else {
+							$pairs[$_account->getId()] = $_account->getName();
+						}
+					}
+
+					$form->addSelect('account', 'Účet', $pairs)  // TODO translate
+						->setPrompt('---')
+						->setRequired();
+				}
+			}
+		}
+	}
 
 	protected function createComponentForm(): Form
 	{
