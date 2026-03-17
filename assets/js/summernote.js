0fb0eb8dbf301298e04f2e111e53417515094068
Maxmilián Holomek <max@appsdevteam.com>
2026-03-17 20:55:33 +0100

    fix(editor): improve Summernote table styling and clean HTML attributes
    - Add consistent 1px borders to Summernote table cells
    - Remove additional unwanted HTML attributes (border, cellpadding, cellspacing, style, width)
    - Restructure CSS to properly scope table styles within note-editor

--- assets/js/summernote.js
+++ assets/js/summernote.js
@@ -64,7 +64,7 @@ $.nette.ext('live').after(function (el) {
 				keepHtml: true,
 				keepTagContents: ['span'], //Remove tags and keep the contents
 				badTags: ['applet', 'col', 'colgroup', 'embed', 'noframes', 'noscript', 'script', 'style', 'title', 'meta', 'link', 'head'], //Remove full tags with contents
-				badAttributes: ['bgcolor', 'height', 'lang', 'start', 'valign', 'data-(.*?)', 'class'], //Remove attributes from remaining tags
+				badAttributes: ['bgcolor', 'border', 'cellpadding', 'cellspacing', 'height', 'lang', 'start', 'style', 'valign', 'width', 'data-(.*?)', 'class'], //Remove attributes from remaining tags
 				limitChars: 0, // 0|# 0 disables option
 				limitDisplay: 'both', // none|text|html|both
 				limitStop: false, // true/false
