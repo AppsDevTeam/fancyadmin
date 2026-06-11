c777cc93c583485916baaf954a5bce51db7dc303
Pavel Konvička <pavel.konvicka@gmail.com>
2026-06-11 23:44:02 +0200

    https://trello.com/c/ZvBbVEg1/5058-umožnit-pridat-podrazenou-kategorii

---
+++ src/Model/Entities/Traits/Tree.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADT\FancyAdmin\Model\Entities\Traits;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Self-referencing strom (adjacency list). Pocita s tim, ze entita je take Gedmo Sortable
+ * (ma position) - parent je oznacen jako SortableGroup, takze razeni je v ramci urovne.
+ * V init() entity zavolej $entity->initTree().
+ */
+trait Tree
+{
+	#[Gedmo\SortableGroup]
+	#[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'children')]
+	#[ORM\JoinColumn(nullable: true, onDelete: 'SET NULL')]
+	protected ?self $parent = null;
+
+	#[ORM\OneToMany(mappedBy: 'parent', targetEntity: self::class)]
+	protected Collection $children;
+
+	protected function initTree(): void
+	{
+		$this->children = new ArrayCollection();
+	}
+
+	public function getParent(): ?static
+	{
+		return $this->parent;
+	}
+
+	public function setParent(?self $parent): static
+	{
+		if ($parent === $this) {
+			$parent = null;
+		}
+		$this->parent = $parent;
+		return $this;
+	}
+
+	/**
+	 * @return static[]
+	 */
+	public function getChildren(): array
+	{
+		return $this->children->toArray();
+	}
+}
