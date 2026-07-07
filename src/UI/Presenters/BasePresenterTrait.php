2f24842a61f94f6a5f4dd8e7c601f3ad95c35265
Maxmilián Holomek <max@appsdevteam.com>
2026-07-07 11:45:36 +0200

    fix deprecated

--- src/UI/Presenters/BasePresenterTrait.php
+++ src/UI/Presenters/BasePresenterTrait.php
@@ -152,7 +152,7 @@ trait BasePresenterTrait
 	public function formatTemplateFiles(): array
 	{
 		$list = parent::formatTemplateFiles();
-		$list[] = __DIR__ . '/' . explode(':', $this->getName())[1] . '/' . $this->view . '.latte';
+		$list[] = __DIR__ . '/' . explode(':', $this->getName())[1] . '/' . $this->getView() . '.latte';
 		return $list;
 	}
 
